b5cd5d83271e74d24093faf728c27004693c5c19
David Dutra <dadutra@hotmail.com>
2024-04-28 08:30:44 -0300

    Board set position, piece[][]

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,18 +1,17 @@
 package boardgame;
 
 public class Board {
-
     private int rows;
-
     private int columns;
-
     private Piece[][] pieces;
+
 
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
     }
+
 
     public int getRows() {
         return rows;
@@ -28,5 +27,13 @@ public class Board {
 
     public void setColumns(int columns) {
         this.columns = columns;
+    }
+
+    public Piece piece(int row, int columns) {
+        return pieces [row][columns];
+    }
+
+    public Piece piece(Position position) {
+        return null;
     }
 }
