0ac81e66f5c39a8c4d7543135ad98a2a7c3d45f3
David Dutra <dadutra@hotmail.com>
2024-04-30 10:17:15 -0300

    Set player turn on ChessMatch

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,4 +1,5 @@
 package application;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -43,6 +44,13 @@ public class UI {
         } catch (RuntimeException e) {
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Qaiting player: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
