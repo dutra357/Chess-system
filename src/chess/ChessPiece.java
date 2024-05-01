4226ba0959633d358b0d4c874059801abf6574aa
David Dutra <dadutra@hotmail.com>
2024-05-01 08:59:50 -0300

    Increase and Decrease move count (ChessPiece and ChessMAtch)

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,11 +6,24 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
     private Color color;
+    private int moveCount;
 
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
     }
 
     public Color getColor() {
