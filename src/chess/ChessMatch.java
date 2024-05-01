23f7692b8ca2f5dc6d140951ffa8cb3d1e912e57
David Dutra <dadutra@hotmail.com>
2024-05-01 08:46:41 -0300

    CheckMate implement (Chessmatch)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -17,6 +17,7 @@ public class ChessMatch {
 
     //Start w false
     private boolean check;
+    private boolean checkMate;
 
     private List<Piece> piecesOnTheBoard = new ArrayList<>();
     private List<Piece> capturedPieces = new ArrayList<>();
@@ -31,6 +32,10 @@ public class ChessMatch {
 
     public boolean getCheck(){
         return check;
+    }
+
+    public boolean getCheckMate(){
+        return checkMate;
     }
 
     public int getTurn() {
@@ -72,6 +77,9 @@ public class ChessMatch {
 
         check = testCheck(opponent(currentPlayer));
 
+        if (testMate(opponent(currentPlayer))) {
+            checkMate = true;
+        }
         nextTurn();
         return (ChessPiece) capturedPiece;
     }
@@ -156,6 +164,33 @@ public class ChessMatch {
             }
         }
         return false;
+    }
+
+    private boolean testMate(Color color) {
+        if (!testCheck(color)) {
+            return false;
+        }
+        List<Piece> list = piecesOnTheBoard.stream()
+                .filter(x -> ((ChessPiece)x).getColor() == color).toList();
+
+        for (Piece p : list) {
+            boolean[][] mat = p.possibleMovies();
+            for (int i = 0; i < board.getRows(); i++) {
+                for (int j = 0; j < board.getColumns(); j++){
+                    if (mat[i][j]) {
+                        Position source = ((ChessPiece)p).getChessPosition().toPosition();
+                        Position target = new Position(i, j);
+                        Piece capturedPiece = makeMove(source, target);
+                        boolean testCheck = testCheck(color);
+                        undoMove(source, target, capturedPiece);
+                        if (!testCheck) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     //Star game, placed the pieces on the board
